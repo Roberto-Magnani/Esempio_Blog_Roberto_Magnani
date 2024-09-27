2bf250b0e0c88fd6e2b79c66ee5f41c0679e007b
Roberto Magnani <r.magnani93@gmail.com>
2024-09-27 13:18:56 +0200

    impostata la tabella database per Classes

--- database/migrations/2024_09_27_104127_create_classes_table.php
+++ database/migrations/2024_09_27_104127_create_classes_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('class');
+            $table->string('pf_dice');
+            $table->string('pf_first');
+            $table->string('pf_higher');
+            $table->string('armors');
+            $table->string('weapons');
+            $table->string('tools');
+            $table->string('saving_throws');
+            $table->string('abilities');
+            $table->string('equipments');
+            $table->string('others')->nullable;
+            //! colonna di uder_id di tipo intero positivo e senza segno
+            $table->unsignedBigInteger('user_id')->nullable();
+            //! chiave col vincolo di integritá referenziale -> foreign che prende lo stesso nome della colonna
+            //! fará riferimento alla colonna id sulla tabella users
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
