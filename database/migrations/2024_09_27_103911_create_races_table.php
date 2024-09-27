c0c34c9a948305deea5cd6f6b3fcc45521a774e8
Roberto Magnani <r.magnani93@gmail.com>
2024-09-27 13:06:08 +0200

    iniziato progetto blog, creato il databasee collegato, creato modello -mrc di Races, creata tabella, impostato i vincoli con user_id

---
+++ database/migrations/2024_09_27_103911_create_races_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('races', function (Blueprint $table) {
+            $table->id();
+            $table->string('img');
+            $table->string('name');
+            $table->string('plot');
+            $table->string('alignment');
+            $table->string('age');
+            $table->string('size');
+            $table->string('speed');
+            $table->string('language');
+            $table->string('subrace');
+            //! colonna di uder_id di tipo intero positivo e senza segno
+            $table->unsignedBigInteger('user_id')->nullable();
+            //! chiave col vincolo di integritá referenziale -> foreign che prende lo stesso nome della colonna
+            //! fará riferimento alla colonna id sulla tabella users
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('races');
+    }
+};
